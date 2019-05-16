c7721029856c31f6736de2b1ec2e7fb5084e1ae7
bj7 <jjbernitt@gmail.com>
2019-05-15 20:45:01 -0400

    setup

---
+++ Magic8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8
+//
+//  Created by Joshua Bernitt on 5/15/19.
+//  Copyright © 2019 Joshua Bernitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
