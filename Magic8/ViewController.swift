81cbeed9c20ebb771ae48767f9131c88bef8fbda
bj7 <jjbernitt@gmail.com>
2019-05-15 21:03:35 -0400

    created basic magic 8ball app App has button and motion detection for changes

--- Magic8/ViewController.swift
+++ Magic8/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        shakeWatYoMamaGaveYa()
+    }
+    
+    func shakeWatYoMamaGaveYa() {
+        let random = Int.random(in: 1...5)
+        ballImageView.image = UIImage(named: "ball\(random)")
     }
 
-
+    @IBAction func askButtonClick(_ sender: Any) {
+        shakeWatYoMamaGaveYa()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        shakeWatYoMamaGaveYa()
+    }
+    
 }
 
